788fb522c2f065d8a57d91c7e27c08fb6f2c8ef5
duncte123 <contact@duncte123.me>
2020-08-24 16:23:14 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ TIMESTAMP=$(date +%s)
 npm ci
 npx grunt
 go build -ldflags="-w -s -X main.VERSION=${BRANCH}.${COMMIT_SHA}.${TIMESTAMP}" -o build/ghostbin
+cp *.yml build/
+rm -rf .tmp
 #go build -ldflags="-w -s -X main.VERSION=1.0.0"
