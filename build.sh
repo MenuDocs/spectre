363b35c92d1ce57c2ae76b742535f0b3f538d462
duncte123 <contact@duncte123.me>
2020-08-24 16:06:55 +0200

    Add propper buildscript

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-go build -ldflags="-w -s -X main.VERSION=1.0.0"
+#!/bin/sh
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+COMMIT_SHA=$(git rev-parse --short HEAD)
+TIMESTAMP=$(date +%s)
+
+npm ci
+npx grunt
+go build -ldflags="-w -s -X main.VERSION=${BRANCH}.${COMMIT_SHA}.${TIMESTAMP}" -o build/ghostbin
+#go build -ldflags="-w -s -X main.VERSION=1.0.0"
