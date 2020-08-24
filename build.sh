177d5e24de7220cc89c13fce35093b93d9601f89
duncte123 <contact@duncte123.me>
2020-08-24 16:29:05 +0200

    Add move script to build

--- build.sh
+++ build.sh
@@ -8,4 +8,12 @@ npx grunt
 go build -ldflags="-w -s -X main.VERSION=${BRANCH}.${COMMIT_SHA}.${TIMESTAMP}" -o build/ghostbin
 cp *.yml build/
 rm -rf .tmp
+
+# move to dest dir if exists
+if [ -d "/home/websites/apps/ghostbin" ]
+then
+    rm -rf /home/websites/apps/ghostbin/build/
+    mv ./build/ /home/websites/apps/ghostbin/
+fi
+
 #go build -ldflags="-w -s -X main.VERSION=1.0.0"
